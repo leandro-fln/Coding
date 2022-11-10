721b1910f7201db4553ad53891496d27b5116aa3
leandrofln83@gmail.com <leandrofln83@gmail.com>
2022-11-09 23:43:52 -0300

    Def to_file

--- Ruby_Translator/translator.rb
+++ Ruby_Translator/translator.rb
@@ -10,4 +10,14 @@ text = gets.chomp
 translator = Translator.new
 text_translated = translator.translate(from, to, text)
 
+def to_file
+    time = Time.new
+    nome_arquivo = time.strftime('%d-%m-%y_%H:%M') + '.txt'
+    File.open(nome_arquivo, 'w') do |line|
+      line.puts text
+      line.puts text_translated
+    end
+end
+
 puts text_translated.text
+to_file
